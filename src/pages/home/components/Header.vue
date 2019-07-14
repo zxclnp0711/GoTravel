eb8eadcf604839ca0629e0e495a3b2e29a3f1bc6
lichenfeng <2277815065@qq.com>
2019-07-14 09:28:16 +0800

    123

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="header">
-      <div class="header-left">返回</div>
-      <div class="header-input">输入城市/景点/游玩/主题</div>
-      <div class="header-right">城市</div>
-    </div>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩/主题</div>
+    <div class="header-right">城市</div>
+  </div>
 </template>
 
 <script>
@@ -13,26 +13,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .header
-    display: flex
-    height: .86rem
-    line-height: .86rem
-    background: #00bcd4
-    color: #fff
-    .header-left
-      width: .64rem
-      float: left
-    .header-input
-      flex: 1
-      height: .64rem
-      line-height: .64rem
-      margin-top: .12rem
-      margin-left: .2rem
-      background: #fff
-      border-radius: .08rem
-      color: #ccc
-    .header-right
-      width: 1.24rem
-      float: right
-      text-align: center
+.header {
+  display: flex;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  background: #00bcd4;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    background: #fff;
+    border-radius: 0.08rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+  }
+}
 </style>
